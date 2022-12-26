8f0a95e774b5f64889442dd41d35ed622fdd9472
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-12-26 15:04:56 +0700

    chore: install dependency and setting configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Segoe UI', 'sans-serif'],
+      serif: ['Nunito Sans', 'serif'],
+    },
+    extend: {
+      screens: {
+        sm: '385px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#4BA485',
+          90: '#E0FCD6',
+          100: '#265B4C',
+        },
+        dark: {
+          DEFAULT: '#131B20',
+          secondary: "#222C32",
+          third: '#2D3941'
+        },
+        grey:{
+          DEFAULT: '#EAECEF',
+          secondary: '#E2E1DD',
+          third: '#F0F2F5'
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
   },
   plugins: [],
-}
+  darkMode: ['class'],
+};
