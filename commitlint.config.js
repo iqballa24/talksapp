98cf1e818132a3508607d1a1104f666f03a7b78f
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-12-29 09:23:07 +0700

    chore(package.json): setup commitizen and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {},
+};
