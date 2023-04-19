582559631db3477dd8afd15636816b2a7db84547
Iqbal Nugraha <tengku.nugraha@erajaya.com>
2023-04-19 21:47:27 +0700

    style(auth layout): change size padding top and bottom on auth header

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -8,7 +8,7 @@ const AuthLayout = () => {
   return (
     <main className={`relative w-full h-[100vh] bg-grey after:fixed after:w-full after:h-56 after:bg-${accentColor} dark:bg-dark-third dark:after:bg-dark-third`}>
       <section className="absolute flex flex-col w-full max-w-4xl h-full top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 z-20">
-        <div className="flex flex-row items-center justify-center lg:justify-start gap-2 py-7">
+        <div className="flex flex-row items-center justify-center lg:justify-start gap-2 py-5">
           <img src="/logo-flat.svg" alt="" className="w-full max-w-[39px]" />
           <h1 className="text-white font-medium">TALKSAPPS WEB</h1>
         </div>
